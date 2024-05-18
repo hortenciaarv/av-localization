ab56d09c604a78d0accce0b5235773831daee8d5
Antonioleon13 <jantonioleon.irs@gmail.com>
2024-05-18 00:35:11 +0000

    Added workspace for ros noetic

---
+++ workspace/puzzlebot_ws/build/puzzlebot_control/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/workspace/puzzlebot_ws/src/puzzlebot_control
+# Build directory: /root/workspace/puzzlebot_ws/build/puzzlebot_control
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ workspace/puzzlebot_ws/build/puzzlebot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/workspace/puzzlebot_ws/src/puzzlebot_description
+# Build directory: /root/workspace/puzzlebot_ws/build/puzzlebot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
