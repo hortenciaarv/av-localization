cf5b481364090761b3f3b093921c7e796f90ed2b
root <andrea@superobotics.com>
2024-05-18 01:13:38 +0000

    packages added

--- README.md
+++
@@ -1,1 +0,0 @@
-# Team_IntegrationRobotics_and_IntelligentSystems
--- catkin_ws/build/localization/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/root/Team_IntegrationRobotics_and_IntelligentSystems/catkin_ws/install/localization'
